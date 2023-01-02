1c548b9c2421079c1f46e63a8a3a9df954717dbf
themx05 <maximiliencomlan05@gmail.com>
2023-01-02 18:38:17 +0100

    fix: updated design adaptivity, added contact drawer

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,10 +1,12 @@
 import classNames from "classnames";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, Ref } from "react";
 import styles from "./index.module.scss";
 
-export function Container(props: HTMLAttributes<HTMLDivElement>) {
+export function Container(
+  {innerRef, ...props}: HTMLAttributes<HTMLDivElement> & { innerRef?: Ref<HTMLDivElement> }
+) {
   return (
-    <div {...props} className={classNames(styles.base, props.className || "")}>
+    <div {...props} ref={innerRef} className={classNames(styles.base, props.className || "")}>
       {props.children}
     </div>
   );
--- src/components/Navigation/Appbar/index.tsx
+++ src/components/Navigation/Appbar/index.tsx
@@ -1,41 +1,137 @@
-import { Desktop, List } from "phosphor-react";
+import { Envelope, List, MapPin, Phone, X } from "phosphor-react";
+import { useState } from "react";
+import { FaLinkedin, FaSlack, FaTwitter, FaYoutube } from "react-icons/fa";
 import { Container } from "../../Container";
+import { InputNewsletter } from "../../Inputs/InputNewsletter";
+import NavigationDrawer from "../drawer";
+import NavItem from "../NavItem";
+import NavItemSurface from "../NavItem/surface";
 import styles from "./index.module.scss";
 
-function DesktopMenu() {
+function DesktopMenu(props: { parent: HTMLDivElement | undefined }) {
+  const services = [
+    "React.js Development",
+    "Digital Marketing",
+    "UI/UX Design",
+  ];
+
   return (
     <div data-variant="desktop" className={styles.menu}>
-      <div className={styles.item}>
+      <NavItem
+        className={styles.item}
+        withSurface
+        surfaceExpanded
+        anchor={props.parent}
+      >
         <span className={styles.title}>Home</span>
-      </div>
-      <div className={styles.item}>
+        <NavItemSurface>
+          <p>Here is the home space</p>
+        </NavItemSurface>
+      </NavItem>
+      <NavItem className={styles.item}>
         <span className={styles.title}>About</span>
-      </div>
-      <div className={styles.item}>
+      </NavItem>
+      <NavItem className={styles.item} withSurface anchor={props.parent}>
         <span className={styles.title}>Services</span>
-      </div>
-      <div className={styles.item}>
+        <NavItemSurface>
+          {services.map((s) => {
+            return (
+              <div key={s} className={styles.service}>
+                <p>{s}</p>
+              </div>
+            );
+          })}
+        </NavItemSurface>
+      </NavItem>
+      <NavItem className={styles.item} anchor={props.parent}>
         <span className={styles.title}>Portfolio</span>
-      </div>
-      <div className={styles.item}>
-        <span className={styles.title}>Blog</span>
-      </div>
+        <NavItemSurface>
+          <p>Our latest projects.</p>
+        </NavItemSurface>
+      </NavItem>
+      <NavItem className={styles.item}>
+        <span className={styles.title}>blog</span>
+      </NavItem>
     </div>
   );
 }
 
 export function Appbar() {
+  const [parentRef, setParentRef] = useState<HTMLDivElement | null>();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
     <div className={styles.appbar}>
-      <Container className={styles.content}>
+      <Container className={styles.content} innerRef={setParentRef}>
         <h1 className={styles.heading}>ArcadiaWeb</h1>
-        <DesktopMenu />
+        <DesktopMenu parent={parentRef ?? undefined} />
         <div className={styles.side_actions}>
-          <button className={styles.hamburger}>
+          <button
+            onClick={() => setDrawerOpen(true)}
+            className={styles.hamburger}
+          >
             <List size={32} />
           </button>
         </div>
       </Container>
+      <NavigationDrawer
+        isOpen={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <div className={styles.menu_drawer}>
+          <button
+            className={styles.close_btn}
+            onClick={() => setDrawerOpen(false)}
+          >
+            <X size={24} />
+          </button>
+
+          <div className={styles.heading}>
+            <h2 className={styles.heading_title}>Arcadiaweb</h2>
+            <p className={styles.heading_description}>
+              Do you have a project in your mind? Keep connect us.
+            </p>
+          </div>
+
+          <div className={styles.contacts}>
+            <h2 className={styles.section_title}>Contact Us</h2>
+            <div className={styles.contact}>
+              <Phone size={24} color="var(--colors-primary)" />
+              <p>+44 454 7800 112</p>
+            </div>
+            <div className={styles.contact}>
+              <Envelope size={24} color="var(--colors-primary)" />
+              <p>infotech@arino.com</p>
+            </div>
+            <div className={styles.contact}>
+              <MapPin size={24} color="var(--colors-primary)"/>
+              <p>50 Wall Street Suite, 44150 Ohio, United States</p>
+            </div>
+          </div>
+          <div className={styles.subscribe}>
+            <h3>Subscribe</h3>
+            <InputNewsletter />
+            <p className={styles.subscription_text}>
+              At vero eos et accusamus et iusto odio as part dignissimos ducimus
+              qui blandit.
+            </p>
+          </div>
+          <div className={styles.social_row}>
+            <button className={styles.social}>
+              <FaLinkedin size={18}/>
+            </button>
+            <button className={styles.social}>
+              <FaTwitter size={18}/>
+            </button>
+            <button className={styles.social}>
+              <FaYoutube size={18}/>
+            </button>
+            <button className={styles.social}>
+              <FaSlack size={18}/>
+            </button>
+          </div>
+        </div>
+      </NavigationDrawer>
     </div>
   );
 }
