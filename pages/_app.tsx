a976368b94c8b2058d0b5c98ee0b6255c61b3c09
themx05 <maximiliencomlan05@gmail.com>
2023-01-04 13:33:48 +0100

    fix: added mouse tracker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
 import appTheme from "../src/theming";
 import { Footer } from "../src/sections/Footer";
+import { MouseTracker } from "../src/components/MouseTracker";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="app">
       <ThemeProvider theme={appTheme}>
         <Component {...pageProps} />
+        <MouseTracker/>
         <Footer />
       </ThemeProvider>
     </div>
